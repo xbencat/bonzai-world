ae9b98c4238692c4c5c41c2b7fcebd6b8854e1d3
Gregor Bencat <gregor.bencat@swida.sk>
2024-03-18 21:05:42 +0100

    init

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'Bonzai World'
+export const appDescription = 'New World company management system'
